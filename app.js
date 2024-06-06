3120a0f8585775550a97a5833319dee82b621ff0
shantanusawant26 <shantanusawant26@gmail.com>
2024-06-06 15:16:32 +0530

    changes

--- app.js
+++ app.js
@@ -2,27 +2,11 @@
 const cors = require('cors');
 const express = require("express");
 const app = express();
-// const conatactUsRouters = require("./contactUsRouters");
-// const applicationFormRouters = require("./applicationFormRouters");
-// const classesRouters = require('./classRouters')
-// const bodyParser = require("body-parser");
-// const subscribeRouters = require("./subscribeRouters")
-// const chatBotRouter = require('./chatBotRouters')
-
-// Define middleware
-app.use(express.json());
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use(cors());
 
 
 
-// app.use(function(req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     next();
-// });
+
+
 app.use("/",(req, res) => {
     res.json("Both userMessage and response fields are required" );
 })
