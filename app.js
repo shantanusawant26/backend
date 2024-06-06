ebab9e9ad259218b6913fd90cd69f6c51a2dedd8
shantanusawant26 <shantanusawant26@gmail.com>
2024-06-06 15:03:29 +0530

    changes

--- app.js
+++ app.js
@@ -16,13 +16,13 @@ app.use(cors());
 
 
 
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     next();
+// });
 app.use("/",(req, res) => {
     res.json("Both userMessage and response fields are required" );
 })
